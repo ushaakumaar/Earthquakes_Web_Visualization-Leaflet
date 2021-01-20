ee17a9b5c92db556950f9a22c44a0a1a51eed2b9
Usha Saravanakumar <ushaakumaar@gmail.com>
2021-01-19 22:53:18 -0500

    User defined function that creates map with earthquake and faultline data

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -34,3 +34,109 @@ function getEarthquakeData() {
         });
     });
 }
+
+// Creates map with earthquake and faultline data
+function createMap(earthquakes, faultlineData) {
+
+    // Defining information to be displayed on clicking the markers on the map
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h2>Magnitude:" + feature.properties.mag + "</h2><h3>" + feature.properties.place +
+            "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    }
+
+    // Creating a GeoJSON layer with the retrieved earthquake data
+    var earthquakeLayer = L.geoJSON(earthquakes, {
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng, {
+                radius: getRadius(feature.properties.mag),
+                fillColor: getColor(feature.properties.mag),
+                color: "black",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 1
+            });
+        },
+        onEachFeature: onEachFeature
+    });
+
+
+    // Creating a GeoJSON layer with the retrieved faultline data
+    var faultlineLayer = L.geoJSON(faultlineData, {
+        color: "#fba200"
+    });
+
+    // Creating base layers
+
+    // satellite Layer
+    var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/satellite-streets-v11",
+        accessToken: API_KEY
+    });
+
+    //outdoor layer
+    var outdoormap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/outdoors-v9",
+        accessToken: API_KEY
+    });
+
+    // grayscale layer
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "dark-v10",
+        accessToken: API_KEY
+    });
+
+
+    // Create a baseMaps object
+    var baseMaps = {
+        "Satellite": satellitemap,
+        "Grayscale": darkmap,
+        "Outdoors": outdoormap
+    };
+
+    // Create an overlay object
+    var overlayMaps = {
+        "Fault Lines": faultlineLayer,
+        "Earthquakes": earthquakeLayer
+    };
+
+    // Create a map object
+    var myMap = L.map("map", {
+        center: [15.5994, -28.6731],
+        zoom: 2,
+        layers: [satellitemap, earthquakeLayer, faultlineLayer]
+    });
+
+    // Pass our map layers into our layer control
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
+
+    // Display legend at bottom right corner of the map
+    var legend = L.control({ position: 'bottomright' });
+    legend.onAdd = function(myMap) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            mags = [0, 1, 2, 3, 4, 5];
+
+        // loop through magnitude intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < mags.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(mags[i]) + '"></i> ' +
+                mags[i] + (mags[i + 1] ? '&ndash;' + mags[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    };
+    legend.addTo(myMap);
+}
