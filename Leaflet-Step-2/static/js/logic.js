44cb49ec593edb765f25ab4fc0440405af20def1
Usha Saravanakumar <ushaakumaar@gmail.com>
2021-01-19 22:51:22 -0500

    User defined function that returns color based on magtitude

---
+++ Leaflet-Step-2/static/js/logic.js
@@ -0,0 +1,13 @@
+/*******************************
+ * USER DEFINED FUNCTIONS
+ *******************************/
+
+// Returns color based on magtitude
+function getColor(magnitude) {
+    return magnitude >= 5 ? '#bd0026' :
+        magnitude >= 4 ? '#f03b20' :
+        magnitude >= 3 ? '#fd8d3c' :
+        magnitude >= 2 ? '#feb24c' :
+        magnitude >= 1 ? '#fed976' :
+        '#ffffb2';
+}
